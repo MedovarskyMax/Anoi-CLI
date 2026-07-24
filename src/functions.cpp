732fb81e56929b270702927a642a13725b96e0f9
notspagheti <notspagheti@gmail.com>
2026-07-24 13:21:27 +0200

    Add: printHelp [anoi -h | anoi --help]

--- src/functions.cpp
+++ src/functions.cpp
@@ -2,7 +2,16 @@
 #include <include/functions.h>
 
 void printHelp(){
+  std::cout << "\033[1mAnoi\033[0m is a lightweight CLI which lets you";
+  std::cout << " \033[32mcreate\033[0m, \033[33medit\033[0m, \033[31mremove\033[0m and \033[96mbrowse\033[0m shortcuts\n\n";
 
+  std::cout << "Usage:  anoi <shortcut>             Opens the <url> saved under <shortcut> in default browser\n";
+  std::cout << "        anoi add <shortcut> <url>   Assigns a <url> to a <shortcut>\n";
+  std::cout << "        anoi edit <shortcut> <url>  Updates the <url> assigned to <shortcut>\n";
+  std::cout << "        anoi rm <shortcut>          Removes <shortcut>\n";
+  std::cout << "        anoi ls                     Lists all saved <shortcut> : <url> combinations\n";
+  std::cout << "        anoi ls -tui                Opens interactive TUI\n";
+  std::cout << "        anoi                        Opens interactive TUI\n";
 }
 
 
