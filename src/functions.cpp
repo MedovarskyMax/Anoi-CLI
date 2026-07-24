8c023a740b077c87ba14714fc513d7b535d654ef
notspagheti <notspagheti@gmail.com>
2026-07-24 23:14:05 +0200

    Update: --help

--- src/functions.cpp
+++ src/functions.cpp
@@ -14,13 +14,12 @@ void printHelp(){
   std::cout << "\033[1mAnoi\033[0m is a lightweight CLI which lets you";
   std::cout << " \033[32mcreate\033[0m, \033[33medit\033[0m, \033[31mremove\033[0m and \033[96mbrowse\033[0m shortcuts\n\n";
 
-  std::cout << "Usage:  anoi <shortcut>             Opens the <url> saved under <shortcut> in default browser\n";
-  std::cout << "        anoi add <shortcut> <url>   Assigns a <url> to a <shortcut>\n";
-  std::cout << "        anoi edit <shortcut> <url>  Updates the <url> assigned to <shortcut>\n";
-  std::cout << "        anoi rm <shortcut>          Removes <shortcut>\n";
-  std::cout << "        anoi ls                     Lists all saved <shortcut> : <url> combinations\n";
-  std::cout << "        anoi ls -tui                Opens interactive TUI\n";
-  std::cout << "        anoi                        Opens interactive TUI\n";
+  std::cout << "Usage:  anoi <shortcut>                      Opens the <url> saved under <shortcut> in default browser\n";
+  std::cout << "        anoi add <shortcut> <url>            Assigns a <url> to a <shortcut>\033[2m, <shortcut> cannot already be defined\033[0m\n";
+  std::cout << "        anoi edit <shortcut> <url>           Updates the <url> assigned to <shortcut>\n";
+  std::cout << "        anoi rm <shortcut> \033[2m[-f | --force]\033[0m    Removes <shortcut>\033[2m, using optional force flag skips remove confirmation\033[0m\n";
+  std::cout << "        anoi ls \033[2m[-t | --tui]\033[0m                 Lists all saved <shortcut> : <url> combinations\033[2m, optional TUI flag opens the list in a TUI\033[0m\n";
+  std::cout << "        anoi \033[2m[-h | --help] [-v | --version]\033[0m  Opens interactive TUI\033[2m, optional help flag shows help & optional version flag display version\033[0m\n";
 }
 
 
